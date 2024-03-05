aa398a43f31a5fb7f6e07d629cd476c862be5904
Luca Casalone <casaloneluca7@gmail.com>
2024-03-05 20:08:40 +0100

    inizio progetto

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Postre</title>
+</head>
+<body>
+    <x-layout>
+<div class="container-fluid p-5 bg-info text-center text-white">
+<h1 display-1> The Postre</h1>
+
+</div>
+
+    </x-layout>
+</body>
+</html>
