d79d6ba6dc8401e7def492f8c7c91bb5903ff940
Arpaia03 <146118139+Arpaia03@users.noreply.github.com>
2024-03-07 19:35:25 +0100

    articles

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,5 +7,19 @@
             </h1>
         </header>
 
+    <p>
+            @foreach($articles as $article)
+        <div class="card" style="width: 18rem;">
+            <img src="{{ Storage::url($article->img) }}" class="card-img-top" alt="{{ $article->title }}">
+            <div class="card-body">
+                <h5 class="card-title">{{ $article->title }}</h5>
+                <p class="card-text">{{ substr($article->description, 0, 20)}}</p>
+                <a href="{{ route('articles.category', $article->category) }}" class="card-text">{{ $article->category→name }}</a>
+                <a href="{{ route('articles.show', $article) }}" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        @endforeach
+    </p>
+
     </div>
 </x-layout>
