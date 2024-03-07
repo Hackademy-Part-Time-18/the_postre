40c8b911e471e50330827101cac51759152c89eb
Luca Casalone <casaloneluca7@gmail.com>
2024-03-07 21:26:47 +0100

    aggiunto messaggio

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,25 +6,11 @@
                 <span class="site-heading-lower">Business Casual</span>
             </h1>
         </header>
-<<<<<<< HEAD
-            <div class="container my-5">
-                <div class="row justify-content-around"> @foreach($articles as $article)
-                    <div class="col-12 col-md-3">
-                        <div class="card">
-                            <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title"> {{$article->title}}</h5>
-                                <p class="card-text">{{$article->subtitle}}</p>
-                                <p class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</p>
-                            </div>
-                            <div class="card-footer text-muted d-flex justify-content-between align-items-center"> Redatto il {{$article->created_at->format('d/m/Y')}} da {{$article->user->name}}
-                            <a href="#" class="btn btn-info text-white">Leggi</a>
-                            </div>
-                        </div>
-                    </div>
-                
-=======
-
+        
+        @if(session('message'))
+        <div class="alert alert-success text-center">{{session('message')}}
+        </div>div 
+        @endif
 
         <div class="container my-5">
             <div class="row justify-content-around"> @foreach($articles as $article)
@@ -40,7 +26,6 @@
                             <a href="{{route('article.show', compact('article'))}}" class="btn btn-info text-white">Leggi</a>
                         </div>
                     </div>
->>>>>>> 0247f6f44486cdf80a8fdd6d452d40ffc06976f8
                 </div>
                 @endforeach
             </div>
