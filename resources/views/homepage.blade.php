ab8a021b5586c0a9db11cea536dfc122881f2d80
Arpaia03 <146118139+Arpaia03@users.noreply.github.com>
2024-03-11 19:09:47 +0100

    ok

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,7 +9,8 @@
 
 
         <div class="container my-5">
-            <div class="row justify-content-around"> @foreach($articles as $article)
+            <div class="row justify-content-around">
+                @foreach($articles as $article)
                 <div class="col-12 col-md-3">
                     <div class="card">
                         <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="...">
