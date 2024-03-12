572a3d34f981abb1b8d8067fa3c2724f362a7705
Vanderley Chaguay <yovaelocone@outlook.com>
2024-03-12 13:18:53 +0100

    regist

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div>
+    {{-- <div>
         <div>
             <h1>register</h1>
         </div>
@@ -19,17 +19,12 @@
                         </ul>
                     </div>
                 @endif
-                <form action="{{ route('register') }}" method="POST">
-                    @csrf
                     <div>
                         <label for="username" class="form-label">Username</label>
                         <input name="name" type="text" class="form-control" id="username" value="{{ old('name') }}">
                     </div>
                     <div>
                         <label for="email" class="form-label">Email</label>
-                        <input name="email" type="email" class="form-control" id="email"
-                            value="{{ old('email') }}">
-                    </div>
                     <div>
                         <label for="password" class="form-label">Password</label>
                         <input name="password" type="password" class="form-control" id="password">
@@ -47,65 +42,61 @@
                 </form>
             </div>
         </div>
-    </div>
-    @section('head')
-    {{-- <p class="tip">Click on button in image container</p>
-    <div class="cont">
-      <div class="form sign-in">
-        <h2>Welcome back,</h2>
-        <label>
-          <span>Email</span>
-          <input type="email" />
-        </label>
-        <label>
-          <span>Password</span>
-          <input type="password" />
-        </label>
-        <p class="forgot-pass">Forgot password?</p>
-        <button type="button" class="submit">Sign In</button>
-        <button type="button" class="fb-btn">Connect with <span>facebook</span></button>
-      </div>
-      <div class="sub-cont">
-        <div class="img">
-          <div class="img__text m--up">
-            <h2>New here?</h2>
-            <p>Sign up and discover great amount of new opportunities!</p>
-          </div>
-          <div class="img__text m--in">
-            <h2>One of us?</h2>
-            <p>If you already has an account, just sign in. We've missed you!</p>
-          </div>
-          <div class="img__btn">
-            <span class="m--up">Sign Up</span>
-            <span class="m--in">Sign In</span>
-          </div>
-        </div>
-        <div class="form sign-up">
-          <h2>Time to feel like home,</h2>
-          <label>
-            <span>Name</span>
-            <input type="text" />
-          </label>
-          <label>
-            <span>Email</span>
-            <input type="email" />
-          </label>
-          <label>
-            <span>Password</span>
-            <input type="password" />
-          </label>
-          <button type="button" class="submit">Sign Up</button>
-          <button type="button" class="fb-btn">Join with <span>facebook</span></button>
-        </div>
-      </div>
-    </div>
-    
-    <a href="https://dribbble.com/shots/3306190-Login-Registration-form" target="_blank" class="icon-link">
-      <img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/256/Dribbble-icon.png">
-    </a>
-    <a href="https://codepen.io/suez/pen/XWyBpre" target="_blank" class="link-footer">New 2023 Version</a>
-    <a href="https://twitter.com/NikolayTalanov" target="_blank" class="icon-link icon-link--twitter">
-      <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/twitter-128.png">
-    </a> --}}
-</x-layout>    
+    </div> --}}
+    <section class="vh-100 bg-image"
+  style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
+  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
 
+              <form action="{{ route('register') }}" method="POST">
+                @csrf
+            <div class="form-outline mb-4">
+                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example1cg">Your Name</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example3cg">Your Email</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cg">Password</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                </div>
+
+                <div class="form-check d-flex justify-content-center mb-5">
+                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                  <label class="form-check-label" for="form2Example3g">
+                    I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
+                  </label>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button type="button"
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                </div>
+
+                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
+                    class="fw-bold text-body"><u>Login here</u></a></p>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,12 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic"
         rel="stylesheet" type="text/css" />
-    <link
-        href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @yield('head')
     <title>The Postre</title>
 
 </head>
