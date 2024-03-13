231e12a2a8ce69eca0a8a9bd9b9df51070a84d3d
Luca Casalone <casaloneluca7@gmail.com>
2024-03-13 20:32:39 +0100

    fine user 3

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class RevisorController extends Controller
@@ -42,5 +43,7 @@ class RevisorController extends Controller
         $article->save();
 
         return redirect()->route('revisor.dashboard');
-    }
+
+   }
+
 }
