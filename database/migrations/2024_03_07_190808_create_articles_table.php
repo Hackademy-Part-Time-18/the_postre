026dddd7ea01cc41fb1f30e08840a8ed2267a809
Vanderley Chaguay <yovaelocone@outlook.com>
2024-03-19 22:28:02 +0100

    json_articles

--- database/migrations/2024_03_07_190808_create_articles_table.php
+++ database/migrations/2024_03_07_190808_create_articles_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
             $table->unsignedBigInteger('category_id')->nullable();
-            $table->foreign('category_id')->references('id')->on('categories')->nullOnDelete();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('SET NULL');
             $table->timestamps();
         });
     }
---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $articlesJson = File::get(database_path('data/articles.json'));
+        $articles = json_decode($articlesJson, true);
+
+        foreach ($articles as $articleData) {
+            Article::create($articleData);
+        }    }
+}
