ad280d9873074aca2fe7a282d898f033c3b7c981
Arpaia03 <146118139+Arpaia03@users.noreply.github.com>
2024-03-21 19:47:55 +0100

    card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,7 +7,34 @@
     </div>
     <div class="card-footer text-muted d-flex justify-content-between align-items-center">
         Redatto il {{ $data }} da {{ $user }}
-        <a href="{{ $url }}"
-            class="btn btn-dark bg-message text-white border-0">Leggi</a>
+        <a href="{{ $url }}" class="btn btn-dark bg-message text-white border-0">Leggi</a>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-4 Services-tab  item">
+            <div class="folded-corner service_tab_1">
+                <div class="text">
+                    <i class="fa fa-image fa-5x fa-icon-image"></i>
+                    <p class="item-title">
+                    <h5 class="card-title">{{ $title }}</h5>
+                    </p>
+                    <p class="card-text">{{ $subtitle }}</p>
+                    <p class="small text-muted fst-italic text-capitalize">{{ $category }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 Services-tab item">
+            <div class="folded-corner service_tab_1">
+                <div class="text">
+                    <i class="fa fa-lightbulb fa-5x fa-icon-image"></i>
+                    <div class="card-footer text-muted d-flex justify-content-between align-items-center">
+                        Redatto il {{ $data }} da {{ $user }} <a href="{{ $url }}" class="btn btn-dark bg-message text-white border-0">Leggi</a>
+                    </div>
+                </div>
+            </div>
+        </div> 
     </div>
 </div>
