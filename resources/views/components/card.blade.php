6c3e8ee09efc67e48c5e5c661bbffcdfae429575
Arpaia03 <146118139+Arpaia03@users.noreply.github.com>
2024-03-21 19:59:44 +0100

    HOMR

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,40 +1,33 @@
-<div class="card h-100 w-100">
-    <img src="{{ Storage::url($image) }}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">{{ $title }}</h5>
-        <p class="card-text">{{ $subtitle }}</p>
-        <p class="small text-muted fst-italic text-capitalize">{{ $category }}</p>
-    </div>
-    <div class="card-footer text-muted d-flex justify-content-between align-items-center">
-        Redatto il {{ $data }} da {{ $user }}
-        <a href="{{ $url }}" class="btn btn-dark bg-message text-white border-0">Leggi</a>
-    </div>
-</div>
 
-<div class="container">
-    <div class="row">
 
-        <div class="col-md-4 Services-tab  item">
-            <div class="folded-corner service_tab_1">
-                <div class="text">
-                    <i class="fa fa-image fa-5x fa-icon-image"></i>
-                    <p class="item-title">
-                    <h5 class="card-title">{{ $title }}</h5>
-                    </p>
-                    <p class="card-text">{{ $subtitle }}</p>
-                    <p class="small text-muted fst-italic text-capitalize">{{ $category }}</p>
-                </div>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<div class="page-content page-container card h-100 w-100" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-sm-12">
+                <!-- Draggable default card start -->
+                <div class="card">
+                    <div class="card-header">
+                        <h5>{{ $title }}</h5>
+                    </div>
+                    <div class="card-block">
+                        <div class="row" id="sortable">
+                            <div class="col-md-3" style="">
+                                <div class="card-sub"> <img class="card-img-top img-fluid" src="https://i.imgur.com/QwIYa8y.jpg" alt="Card image cap">
+                                    <div class="card-block">
+                                        <h4 class="card-title">{{ $subtitle }}</h4>
+                                        <p class="card-text">{{ $category }}</p>
+                                        <div class="card-footer text-muted d-flex justify-content-between align-items-center">
+                                            Redatto il {{ $data }} da {{ $user }}
+                                            <a href="{{ $url }}" class="btn btn-dark bg-message text-white border-0">Leggi</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- Draggable default card start -->
             </div>
         </div>
-        <div class="col-md-4 Services-tab item">
-            <div class="folded-corner service_tab_1">
-                <div class="text">
-                    <i class="fa fa-lightbulb fa-5x fa-icon-image"></i>
-                    <div class="card-footer text-muted d-flex justify-content-between align-items-center">
-                        Redatto il {{ $data }} da {{ $user }} <a href="{{ $url }}" class="btn btn-dark bg-message text-white border-0">Leggi</a>
-                    </div>
-                </div>
-            </div>
-        </div> 
     </div>
 </div>
