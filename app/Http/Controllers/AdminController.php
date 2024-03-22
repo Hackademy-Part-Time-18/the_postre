d884a5ddb965556fc0a7e8beebefa5285612d3d1
Vanderley Chaguay <yovaelocone@outlook.com>
2024-03-22 19:22:04 +0100

    wer

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,9 +11,9 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-        $adminRequests = User::where('is_admin', NULL)->get();
-        $revisorRequests = User::where('is_revisor', NULL)->get();
-        $writerRequests = User::where('is_writer', NULL)->get();
+        $adminRequests = User::where('is_admin', false)->get();
+        $revisorRequests = User::where('is_revisor', false)->get();
+        $writerRequests = User::where('is_writer', false)->get();
         
         return view('admin.dashboard', compact('adminRequests', 'revisorRequests', 'writerRequests'));
     }
